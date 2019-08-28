2ff752291abf4ec423e62db8a0cf82288c46e59e
unknown <contact@workin.co.il>
2019-08-28 21:18:36 +0300

    localhost fix

---
+++ views/precache-manifest.c7d8033502a27c66aeaf636c4c1801a7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cbf837b8a7ea2baa883a3ed788bf8cbd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b298cac8c4f0e29f14de",
+    "url": "/static/css/main.0c48f179.chunk.css"
+  },
+  {
+    "revision": "c3bbdfe8dd249f05c507",
+    "url": "/static/js/2.49f62c2a.chunk.js"
+  },
+  {
+    "revision": "b298cac8c4f0e29f14de",
+    "url": "/static/js/main.b6a76013.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
